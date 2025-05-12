87de7388accbbc6cd86bdda223442d1c8a6b96db
Lina Gromakova <linaWebDev@outlook.com>
2025-05-12 23:15:49 +0300

    Add layout product-item and loading

--- src/projects/comfy-store/comfy-store.jsx
+++ src/projects/comfy-store/comfy-store.jsx
@@ -1,13 +1,15 @@
 import { Header } from './components/header';
+import { Product } from './components/product';
 import { About } from './pages/about';
 import { Home } from './pages/home';
 
 import './style.css';
+import { Loading } from './uikit/loading';
 export function ComfyStore() {
   return (
     <>
       <Header />
-      <Home></Home>
+      <Loading></Loading>
     </>
   );
 }
---
+++ src/projects/comfy-store/uikit/loading.jsx
@@ -0,0 +1,11 @@
+export function Loading() {
+  return (
+    <div className='loading-container'>
+      <div className='loading-body'>
+        <div className='loading-child'></div>
+         <div className='loading-child'></div>
+        <div className='loading-child'></div>
+      </div>
+    </div>
+  );
+}
