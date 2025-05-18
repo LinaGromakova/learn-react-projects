b1257eba71b3f1f780383615e37b12269e99a56d
Lina Gromakova <linaWebDev@outlook.com>
2025-05-19 00:11:50 +0300

    Add values in select (page products), create custom range-input, add button features switch card mode, add data products in products page

--- src/projects/comfy-store/components/card.jsx
+++ src/projects/comfy-store/components/card.jsx
@@ -1,11 +1,15 @@
 import { Link } from 'react-router';
-export function Card({ img, title, price, id }) {
+export function Card({ img, title, price, id, type = 'basic', company }) {
   return (
     <Link to={`/product/${id}`} className='card-link'>
-      <article className='card-item'>
+      <article className={`card-item ${type}`}>
         <img src={img} alt={title} className='card-img' />
         <div className='card-main'>
-          <h2 className='card-title'>{title}</h2>
+          <div className='card-titles'>
+            <h2 className='card-title'>{title}</h2>
+            {company && type === 'row' && <h3 className='card-company'>{company}</h3>}
+          </div>
+
           <span className='card-price'>${price}</span>
         </div>
       </article>
--- src/projects/comfy-store/components/product-filter.jsx
+++ src/projects/comfy-store/components/product-filter.jsx
@@ -1,5 +1,16 @@
+import { useState } from 'react';
 import { GoTriangleDown } from 'react-icons/go';
-export function ProductFilter() {
+
+export function ProductFilter({ companies, categories }) {
+  const [rangeValue, setRangeValue] = useState(0);
+  let priceValue;
+  if (rangeValue == 0) {
+    priceValue = '0.00';
+  } else if (rangeValue == 1000000) {
+    priceValue = '1,000.000';
+  } else {
+    priceValue = parseInt(rangeValue).toLocaleString('id').slice(0, -1);
+  }
   return (
     <form className='form-search' action='#' onSubmit={(e) => e.preventDefault()}>
       <label className='label-search'>
@@ -10,16 +21,14 @@ export function ProductFilter() {
         select category
         <div className='wrapper-select'>
           <GoTriangleDown className='icon-select'></GoTriangleDown>
-          <select name='' id='' className='filter-select'>
-            <option value='' className='filter-option'>
-              1
-            </option>
-            <option value='' className='filter-option'>
-              1
-            </option>
-            <option value='' className='filter-option'>
-              1
-            </option>
+          <select name='categories' id='categories' className='filter-select'>
+            {categories.map((category, index) => {
+              return (
+                <option value={category} key={index}>
+                  {category}
+                </option>
+              );
+            })}
           </select>
         </div>
       </label>
@@ -27,10 +36,14 @@ export function ProductFilter() {
         select company
         <div className='wrapper-select'>
           <GoTriangleDown className='icon-select'></GoTriangleDown>
-          <select name='' id='' className='filter-select'>
-            <option value='' className='filter-option'>
-              1
-            </option>
+          <select name='company' id='company' className='filter-select'>
+            {companies.map((company, index) => {
+              return (
+                <option value={company} key={index}>
+                  {company}
+                </option>
+              );
+            })}
           </select>
         </div>
       </label>
@@ -38,19 +51,36 @@ export function ProductFilter() {
         sort by
         <div className='wrapper-select'>
           <GoTriangleDown className='icon-select'></GoTriangleDown>
-          <select name='' id='' className='filter-select'>
-            <option value='' className='filter-option'>
-              1
-            </option>
+          <select name='sort' id='sort' className='filter-select'>
+            <option value='a-z'>a-z</option>
+            <option value='z-a'>z-a</option>
+            <option value='high'>high</option>
+            <option value='low'>low</option>
           </select>
         </div>
       </label>
       <label className='label-search'>
         <div className='label-row'>
           <span> select price</span>
-          <span>$1,000.00</span>
+          <span>${priceValue}</span>
         </div>
-        <input type='range' name='' id='' />
+        <div className='range-wrapper'>
+          <div className='range-bar' style={{ width: `${Math.max(rangeValue / 10000, 8.3)}%` }}>
+            <div className='range-thumb'></div>
+          </div>
+          <div className='range-track'></div>
+          <input
+            type='range'
+            name='range-price'
+            id='range-price'
+            value={rangeValue}
+            min='0'
+            max='1000000'
+            step='10000'
+            onChange={(e) => setRangeValue(e.target.value)}
+          />
+        </div>
+
         <div className='label-row'>
           <span>0</span>
           <span>Max : $1,000.00</span>
@@ -58,7 +88,7 @@ export function ProductFilter() {
       </label>
       <label className='label-search'>
         free shipping
-        <input type='checkbox' name='' id='' />
+        <input type='checkbox' name='free-shipping' id='shipping' />
       </label>
       <button type='button' className='btn-apply search'>
         search
