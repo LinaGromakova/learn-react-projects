e2646f558a7571b8e6e8c39fecb5fe11f2cdc6ba
Lina Gromakova <linaWebDev@outlook.com>
2025-05-20 23:10:16 +0300

    Add data fetch in orders and process date

--- src/projects/comfy-store/pages/orders.jsx
+++ src/projects/comfy-store/pages/orders.jsx
@@ -1,12 +1,36 @@
+import { useEffect, useState } from 'react';
 import { Pagination } from '../uikit/pagination';
 
 export function Orders() {
+  const [orders, setOrders] = useState({ orders: [], meta: [] });
+  useEffect(() => {
+    fetch('https://strapi-store-server.onrender.com/api/orders', {
+      headers: {
+        authorization:
+          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwiaWF0IjoxNzQ3NzUzMDU3LCJleHAiOjE3NTAzNDUwNTd9.3zF60uNfJdiBPTWG_d9xzbOMQz-hnya2sCbtGbwMkTY',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => setOrders({ orders: data.data, meta: data.meta }));
+  }, []);
+
+  function getDateFormat(hours) {
+    let hour = hours.slice(0, 2);
+    let min = hours.slice(-3);
+    let str = hour >= 12 ? '0' + (hour - 12) + min + ' pm' : hour + min + ' am';
+    return str.length === 9 ? str.slice(1) : str;
+  }
+
+  function getDate(date) {
+    return date[0] + (' ' + date[1] + 'th, ') + date[2];
+  }
+  console.log(orders.orders);
   return (
     <section className='orders-page'>
       <div className='container'>
         <h2 className='title_main'>Your Orders</h2>
         <div className='line-decoration'></div>
-        <p className='orders-total'>total orders : 8611</p>
+        <p className='orders-total'>total orders : {orders.meta.pagination?.total} </p>
         <table className='table-orders'>
           <thead>
             <tr>
@@ -18,20 +42,21 @@ export function Orders() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>Giggle Gary</td>
-              <td>Wit Way, Snickerston</td>
-              <td>1</td>
-              <td>$378.99</td>
-              <td>04:49 pm - May 14th, 2025</td>
-            </tr>
-            <tr>
-              <td>Giggle Gary</td>
-              <td>Wit Way, Snickerston</td>
-              <td>1</td>
-              <td>$378.99</td>
-              <td>04:49 pm - May 14th, 2025</td>
-            </tr>
+            {orders.orders.map((order) => {
+              const { name, address, numItemsInCart, orderTotal, createdAt } = order.attributes;
+              const date = new Date(createdAt) + '';
+              const arrDate = date.slice(4, 21).split(' ');
+              const time = getDateFormat(arrDate[3]) + ' - ' + getDate(arrDate);
+              return (
+                <tr key={order.id}>
+                  <td>{name}</td>
+                  <td>{address}</td>
+                  <td>{numItemsInCart}</td>
+                  <td>{orderTotal}</td>
+                  <td>{time}</td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
         <Pagination></Pagination>
